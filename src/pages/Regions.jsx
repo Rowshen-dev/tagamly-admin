932f5be3af58159da85be34de6378c50ed0a3ddd
Rowshen-dev <hojageldiyev@icloud.com>
2026-05-31 17:37:27 +0500

    feat: regions add

---
+++ src/pages/Regions.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import { supabase } from '../lib/supabase';
+
+export default function Regions() {
+    const [regions, setRegions] = useState([]);
+    const [showRegions, setShowRegions] = useState(false);
+    const [newRegions, setNewRegions] = useState('')
+    useEffect(() => {
+        supabase.from('regions').select('*').then(({data, error}) => {
+            if(error) {
+                console.log('Ошибка')
+                return
+            } 
+            if(data) setRegions(data)
+        })
+    }, [])
+
+    const handleAdd = async () => {
+        await supabase.from('regions').insert ({
+            name: newRegions
+        })
+        const {data} = await supabase.from('regions').select('*')
+        setRegions(data)
+        setShowRegions(false)
+    }
+    return (
+        <div>
+            <div className='header'>
+        <h1>Регионы</h1>
+        <button onClick={() => setShowRegions(true)}>+ Добавить регионы</button>
+        </div>
+        {showRegions && (
+            <div>
+                <input
+                placeholder='Регион'
+                value={newRegions}
+                onChange={(e) => setNewRegions(e.target.value)}
+                />
+                <button onClick={handleAdd}>Сохранить</button>
+                <button onClick={() => setShowRegions(false)}>Отмена</button>
+            </div>
+        )}
+        <table>
+            <tbody>
+                {regions.map((item) => (
+                    <tr key={item.id}>
+                        <td>
+                            {item.name}
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        </div>
+    )
+}
