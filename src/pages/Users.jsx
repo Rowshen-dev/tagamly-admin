5ae7536b9c412e33df229ead911d846938aaf576
Rowshen-dev <hojageldiyev@icloud.com>
2026-06-01 18:26:23 +0500

    feat: users page add

---
+++ src/pages/Users.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import { supabase } from "../lib/supabase";
+
+export default function Users() {
+    const [users, setUsers] = useState([]);
+    const [showUsers, setShowUsers] = useState(false);
+    const [newUsers, setNewUsers] = useState('');
+    useEffect(() => {
+        supabase.from('users').select('*').then(({data, error}) => {
+            if(error) {
+                console.log('Ошибка')
+                return
+            }
+            if(data) setUsers(data)
+        })
+    }, [])
+    const handleAdd = async () => {
+        await supabase.from('users').insert({
+            name: newUsers
+        })
+        const {data} = await supabase.from('users').select('*')
+        setUsers(data)
+        setShowUsers(false)
+    }
+    return (
+        <div>
+            <div className="header">
+        <h1>Пользователи</h1>
+        <button onClick={() => setShowUsers(true)}>+ Добавить пользователя</button>
+        </div>
+        {showUsers && (
+            <div className="modal">
+                <input
+                placeholder='Пользователь'
+                value={newUsers}
+                onChange={(e) => setNewUsers(e.target.value)}
+                />
+                <button onClick={handleAdd}>Сохранить</button>
+                <button onClick={() => setShowUsers(false)}>Отмена</button>
+                </div>
+        )}
+        <table>
+            <tbody>
+                {users.map((item) => (
+                    <tr key={item.id}>
+                        <td>
+                            {item.name}
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        </div>
+    )
+}
