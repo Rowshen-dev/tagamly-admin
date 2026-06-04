34d86e5de9f984193f00fdd70dc53b58a7e5164c
Rowshen-dev <hojageldiyev@icloud.com>
2026-06-04 22:20:05 +0500

    fix: main

--- src/pages/Translations.jsx
+++ src/pages/Translations.jsx
@@ -1,7 +1,44 @@
+import {useState, useEffect} from 'react';
+import { supabase } from '../lib/supabase';
+
 export default function Translations() {
+    const [translations, setTranslations] = useState([]);
+    const [showtranslate, setShowTranslate] = useState(false);
+
+    useEffect(() => {
+        supabase.from('translations').select('*').then(({data, error}) => {
+            if(error) {
+                console.log('Ошибка')
+                return
+            }
+            if(data) setTranslations(data)
+        })
+    }, [])
+
     return (
         <div>
+            <div className='header'>
             <h1>Переводы</h1>
+            <button onClick={() => setShowTranslate(true)}>+ Добавить перевод</button>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>
+                            Ключ
+                        </th>
+                        <th>
+                            Русский
+                        </th>
+                        <th>
+                            Turkmen
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
         </div>
     )
 } 
