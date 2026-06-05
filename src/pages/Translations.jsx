d45d5c81873452d6eb67adb776a452167c1d5780
Rowshen-dev <hojageldiyev@icloud.com>
2026-06-05 17:58:18 +0500

    fix: translations page

--- src/pages/Translations.jsx
+++ src/pages/Translations.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 import { supabase } from '../lib/supabase';
 
+
 export default function Translations() {
     const [translations, setTranslations] = useState([]);
     const [showtranslate, setShowTranslate] = useState(false);
@@ -36,7 +37,13 @@ export default function Translations() {
                     </tr>
                 </thead>
                 <tbody>
-
+                    {translations.map((item) => (
+                        <tr key={item.id}>
+                           <td>{item.key}</td>
+                           <td>{item.ru}</td>
+                           <td>{item.tk}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
