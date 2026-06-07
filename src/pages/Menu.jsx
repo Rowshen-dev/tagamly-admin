7fc6db2808456d4261d7a6f3caba4e3b9c4c375c
Rowshen-dev <hojageldiyev@icloud.com>
2026-06-07 17:06:20 +0500

    fix: add establishments

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,7 +1,26 @@
+import { supabase } from "../lib/supabase";
+import { useState, useEffect } from "react";
+
+
 export default function Menu() {
+    const [menu, setMenu] = useState([]);
+   useEffect(() => {
+        supabase.from('establishments').select('*'). then(({data, error}) => {
+            if (error) {
+                console.log('Ошибка:', error)
+                return
+            }
+            if (data) setMenu(data)
+        })
+    }, [])
     return (
         <div>
             <h1>Рестораны</h1>
+           {menu.map((item) => (
+    <div key={item.id}>
+        {item.name}
+    </div>
+))}
         </div>
     )
 }
