4e7d4b30b9683890d461ce39f2d16066f6fdd237
Rowshen-dev <hojageldiyev@icloud.com>
2026-06-12 16:47:08 +0500

    fix: restaurantspage

--- src/pages/RestaurantsPage.jsx
+++ src/pages/RestaurantsPage.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
 export default function RestaurantsPage() {
-    const [restaurantPage, setRestaurantPage] = useState([]);
+    const [restaurantPage, setRestaurantPage] = useState(null);
     const navigate = useNavigate();
     const { subdomain } = useParams();
 
@@ -20,25 +20,12 @@ export default function RestaurantsPage() {
 
 
     return (
-        <div>
-            <div className='header'>
-            <h1>Kategoriyalar</h1>
-            <button onClick={() => navigate(-1)}> - </button>
-            </div>
-            
-            <div className='grid'>
-                {restaurantPage.map((item) => (
-                    <div key={item.id} className='card'>
-                        <div className='card-emage'>
-                            {item.logo_url && <img src={item.logo_url} alt={item.name} />} 
-                            </div>
-                            {item.name}
-                            </div>
-                ))}
-            </div>
-
-
+    <div>
+        <div className='header'>
+            <h1>{restaurantPage?.name}</h1>
+            <button onClick={() => navigate(-1)}>Назад</button>
         </div>
-    );
+    </div>
+)
 
 }
