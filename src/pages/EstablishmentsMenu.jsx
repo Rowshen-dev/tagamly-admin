aa732611f7bef88e7b1ca925025627eb96977572
Rowshen-dev <hojageldiyev@icloud.com>
2026-06-14 18:50:26 +0500

    fix: update EstablishmentsMenu

--- src/pages/EstablishmentsMenu.jsx
+++ src/pages/EstablishmentsMenu.jsx
@@ -1,5 +1,30 @@
-export default function establishmentsMenu() {
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { supabase } from "../lib/supabase";
+
+export default function EstablishmentsMenu() {
+  const [category, setCategory] = useState([]);
+  
+  const { id } = useParams();
+
+  useEffect(() => {
+    supabase.from('categories')
+    .eq('establishment_id', id)
+    .select('*')
+    .then(({data, error}) => {
+      if(error) {
+        console.log('Ошибка')
+        return
+      }
+      if(data) setCategory(data)
+    })
+  }, []);
+
+
   return (
+    <div>
     <h1>Меню заведения</h1>
+    <div>Заведения: {id}</div>
+    </div>
   )
 }
