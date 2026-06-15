1d6aa782af2d0723d768391561c22df3096709d3
Rowshen-dev <hojageldiyev@icloud.com>
2026-06-15 17:29:04 +0500

    fix: establishmentMenu

--- src/pages/EstablishmentsMenu.jsx
+++ src/pages/EstablishmentsMenu.jsx
@@ -4,8 +4,23 @@ import { supabase } from "../lib/supabase";
 
 export default function EstablishmentsMenu() {
   const [category, setCategory] = useState([]);
-  
+  const [showForm, setShowForm] = useState(false);
+  const [newName, setNewName] = useState('');
+
   const { id } = useParams();
+
+  const handleAdd = async () => {
+    await supabase.from('categories')
+    .insert({
+      name: newName,
+      establishment_id: id
+       })
+       const { data } = await supabase.from('categories').select('*').eq('establishment_id', id)
+       setCategory(data)
+       setShowForm(false)
+      }
+  
+  
 
   useEffect(() => {
     supabase.from('categories')
@@ -25,6 +40,24 @@ export default function EstablishmentsMenu() {
     <div>
     <h1>Меню заведения</h1>
     <div>Заведения: {id}</div>
+    <button onClick={() => setShowForm(true)}>+ Добавить категорию</button>
+    {showForm && (
+      <div>
+        <input
+        placeholder="Название категории"
+        value={newName}
+        onChange={(e) => setNewName(e.target.value)}
+        />
+        <button onClick={handleAdd}>Сохранить</button>
+        <button onClick={() => setShowForm(false)}>Отмена</button>
+    </div>
+    )}
+    {category.map((item) => (
+      <tr key={item.id}>
+        <td>{item.name}</td>
+        <td>{id}</td>
+      </tr>
+    ))}
     </div>
   )
 }
